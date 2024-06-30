151d161c1428c9289a52c9039aa812caa50e1280
mark18cosmic <kaish018@gmail.com>
2024-06-30 05:28:10 +0500

    wip: landing page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,45 @@
+import { Button } from '@nextui-org/react';
+import Link from 'next/link';
 import React from 'react'
+import { PiFlowArrowBold } from "react-icons/pi";
+import { FaGithub } from "react-icons/fa";
 
-const Navbar = () => {
+interface NavLinks {
+    title: string,
+    route: string,
+}
+
+const NavLinks = [
+    {
+        title: "Home",
+        route: "/"
+    },
+    {
+        title: "About",
+        route: "/"
+    },
+    {
+        title: "Contact",
+        route: "/"
+    },
+]
+
+const Navbar = (title:string, route:string) => {
     return (
-        <div className='p-5'>
-            <h1 className='text-white text-2xl font-bold'>TaskFlow</h1>
+        <div className='p-5 flex flex-row items-center justify-between shadow-sm'>
+            <div className=''>
+                <h1 className=' text-2xl flex flex-row gap-2 font-bold'><PiFlowArrowBold className='text-[#6E24A8]' size={30} /> TaskFlow</h1>
+            </div>
+            <div className='w-1/4'>
+                <ul className='hidden md:flex flex-row gap-5'>
+                    {NavLinks.map((NavLink) => (
+                        <li><Link href={NavLink.route}>{NavLink.title}</Link></li>
+                    ))}
+                </ul>
+            </div>
+            <div className=''>
+            <FaGithub className='text-[#6E24A8]' size={35}/>
+            </div>
         </div>
     )
 }
