5fffb4069625dd93ea829fe2fafc4b7565781a3f
mark18cosmic <kaish018@gmail.com>
2024-06-30 22:18:26 +0500

    fix: removed eslinting rules for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,18 @@ export default function Home() {
       <div className="flex flex-col md:flex-row gap-5 items-center">
         {/* Title and description */}
         <div className=" space-y-3 md:w-1/2">
-          <h2 className="font-bold text-2xl md:text-4xl">Manage Projects with <span className="bg-[#6E24A8] text-white">Precision</span> and <span className="bg-[#6E24A8] text-white">Flexibility</span></h2>
-          <p>Transform your project management with our agile kanban board designed to streamline workflows and enhance collaboration. Whether you're a startup, a growing team, or an established enterprise.</p>
-          <Button className="hidden md:flex text-white  font-semibold bg-[#6E24A8] text-lg w-40 ">Start Now</Button>
+          <h2 className="font-bold text-4xl md:text-5xl">Streamline Projects with <span className="bg-[#6E24A8] text-white">Precision</span> and <span className="bg-[#6E24A8] text-white">Flexibility</span></h2>
+          <p className="text-lg leading-normal">Transform your project management with our agile kanban board designed to manage workflows and enhance collaboration. Whether you're a startup, a growing team, or an established enterprise.</p>
+          <div className="hidden items-center md:flex flex-col gap-2">
+            <Button className=" text-white rounded-lg  font-semibold bg-[#6E24A8] text-lg w-full ">Start Now</Button>
+            <p className="text-gray-600 font-thin">This project is a work in progress</p>
+          </div>
         </div>
         {/* Image */}
         <div className="md:w-1/2">
           <Image className="rounded-xl shadow-xl" src={landingpage} alt="LandingPageImage" />
         </div>
-        <Button className="md:hidden flex text-white font-semibold bg-[#6E24A8] w-32">Start Now</Button>
+        <Button className="md:hidden flex text-white font-semibold bg-[#6E24A8] w-full">Start Now</Button>
       </div>
 
     </div>
